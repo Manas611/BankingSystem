e22e5ba56cf5ee657586278a19bb74400e6257b3
Manas Khanna <85574835+Manas611@users.noreply.github.com>
2024-02-14 18:40:27 +0530

    BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -1,15 +1,19 @@
 import java.util.ArrayList;
 class BankAccount {
+    
     private final int AccountNumber;
     private int balance;
+    // List to store the Transaction of a specific customer
     ArrayList<Transaction> transactionHistory;
-    
+
+    // Constructor of BankAccount
     public BankAccount(int accountNumber, int balance) {
         AccountNumber = accountNumber;
         this.balance = balance;
         transactionHistory = new ArrayList<>();
     }
 
+    // Getters of the class
     public int getAccountNumber() {
         return AccountNumber;
     }
@@ -23,6 +27,7 @@ class BankAccount {
         this.balance = balance;
     }
 
+    // Method to show all transactions of a customer
     public void showTransaction()
     {
         for(int i=0;i<transactionHistory.size();i++)
