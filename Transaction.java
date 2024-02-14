3c44346ebc4e710a87b3c882e1cc98528cbc42e2
Manas Khanna <85574835+Manas611@users.noreply.github.com>
2024-02-14 18:50:54 +0530

    Transaction.java

--- Transaction.java
+++ Transaction.java
@@ -3,13 +3,16 @@ class Transaction
     private int transactionId;
     private String transactionType;
     private int amount;
-    
+
+    // constructor for the transaction which sets the value
     public Transaction(int transactionId, String transactionType, int amount) {
         this.transactionId = transactionId;
         this.transactionType = transactionType;
         this.amount = amount;
     }
 
+
+    // Getter of the transaction class for the values to be used
     public int getTransactionId() {
         return transactionId;
     }
