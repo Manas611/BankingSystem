2cda16668428e2e7e42cfcd66979930d86170008
Manas Khanna <85574835+Manas611@users.noreply.github.com>
2024-02-14 18:41:36 +0530

    BankException.java

--- BankException.java
+++ BankException.java
@@ -1,6 +1,8 @@
 
+// Class to handle the Exceptions of different types
 class BankException extends Exception
 {
+    // Calling the constructor for this class
     BankException(String msg)
     {
         super(msg);
